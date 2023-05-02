9c83420f97b51236aae9992d3a6b29a687692769
JoseCabrejoVillarCampus <cabre920903@hotmail.com>
2023-05-02 07:07:02 -0500

    feat: :sparkles: Agregacio ws y module
    agregamos modulacion y workers

--- app.js
+++ app.js
@@ -1,101 +1,48 @@
-document.addEventListener("DOMContentLoaded", () => {
-    getData()
-})
-let offset = 0;
-async function getData() {
-    const url = `https://free-to-play-games-database.p.rapidapi.com/api/filter?tag=3d.mmorpg.fantasy.pvp&platform=pc&offset=${offset}`;
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': 'cc2e083b58msh94959c04b8fc1c4p1017e1jsna6f447753c4e',
-            'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
-        }
-    };
-
-    try {
-        const response = await fetch(url, options);
-        const data = await response.json();
-        console.log(data);
-        showGame(data);
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-function showGame(games, limit = 1) {
+function showGame() {
     const contenedor = document.querySelector('#contenidoA')
-    contenedor.innerHTML = "";
-    games.slice(0, limit).forEach(game => {
-        const {
-            developer,
-            freetogame_profile_url,
-            genre,
-            id,
-            platform,
-            publisher,
-            release_date,
-            short_description,
-            thumbnail,
-            title
-        } = game;
-        const div = document.createElement('div');
-        div.innerHTML = `
-        <div>${title}</div>
-        <div><img src="${thumbnail}"></div>
-        <div>${short_description}</div>
-        <div>${publisher}</div>
-        <div>${release_date}</div>
-        <div>${platform}</div>
-        <div>${genre}</div>
-        <div>${id}</div>
-        <div>${developer}</div>
-        <div><a href="${freetogame_profile_url}">LINK AL JUEGO</a></div><br>`;
-
-        contenedor.appendChild(div)
-    });
+    const ws = new Worker('storage/ws.js');//Instanciamos el worker con la ruta del storage
+    ws.postMessage({data: 'se envio el mensaje'})//enviamos un mesaje al worker, si no lo enviamos no funciona, la data depende de donde sale la informacion, en este caso la data va directamente al worker por esto no la redirigimos
+    ws.addEventListener('message', e => {
+        contenedor.innerHTML = "";
+        contenedor.innerHTML = e.data//insertamos la data
+    })
 }
 
-/* let anterior = document.getElementById('but1');
-anterior.addEventListener('click', async () => {
-    offset -= 1; // Actualizar el offset
-    await getData();
-});
+showGame()//llamamos la funcion
 
-let siguiente = document.getElementById('but');
-siguiente.addEventListener('click', async () => {
-    offset += 1; // Actualizar el offset
-    await getData();
-}); */
-document.addEventListener("DOMContentLoaded", () => {
-    const formulario = document.querySelector("#formulario");
-    const contenedor = document.querySelector("#contenidoA");
+/* ;
 
-    formulario.addEventListener("submit", async (e) => {
-        e.preventDefault();
-        contenedor.innerHTML = '';
-        const searchTerm = document.querySelector("#searchTerm").value;
-        console.log(searchTerm);
-        formulario.reset();
+ */
+// document.addEventListener("DOMContentLoaded", () => {
+//     const formulario = document.querySelector("#formulario");
+//     const contenedor = document.querySelector("#contenidoA");
 
-        if (searchTerm) {
-            try {
-                const searchResponse = await fetch(
-                    `https://free-to-play-games-database.p.rapidapi.com/api/games?search=${searchTerm}`, {
-                        method: "GET",
-                        headers: {
-                            "X-RapidAPI-Key": "cc2e083b58msh94959c04b8fc1c4p1017e1jsna6f447753c4e",
-                            "X-RapidAPI-Host": "free-to-play-games-database.p.rapidapi.com",
-                        },
-                    }
-                );
-                const searchData = await searchResponse.json();
-                console.log(searchData);
-                showGame(searchData);
-            } catch (error) {
-                console.log(error);
-            }
-        } else {
-            getData();
-        }
-    });
-});
+//     formulario.addEventListener("submit", async (e) => {
+//         e.preventDefault();
+//         contenedor.innerHTML = '';
+//         const searchTerm = document.querySelector("#searchTerm").value;
+//         console.log(searchTerm);
+//         formulario.reset();
+
+//         if (searchTerm) {
+//             try {
+//                 const searchResponse = await fetch(
+//                     `https://free-to-play-games-database.p.rapidapi.com/api/games?search=${searchTerm}`, {
+//                         method: "GET",
+//                         headers: {
+//                             "X-RapidAPI-Key": "cc2e083b58msh94959c04b8fc1c4p1017e1jsna6f447753c4e",
+//                             "X-RapidAPI-Host": "free-to-play-games-database.p.rapidapi.com",
+//                         },
+//                     }
+//                 );
+//                 const searchData = await searchResponse.json();
+//                 console.log(searchData);
+//                 showGame(searchData);
+//             } catch (error) {
+//                 console.log(error);
+//             }
+//         } else {
+//             getData();
+//         }
+//     });
+// });
--- storage/ws.js
+++ storage/ws.js
@@ -1,10 +1,26 @@
-import api from "../API/api";
+let offset = 0;
+let ws = {
+    async consult() {
+        const url = `https://free-to-play-games-database.p.rapidapi.com/api/filter?tag=3d.mmorpg.fantasy.pvp&platform=pc&offset=${offset}`;
+        const options = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': 'cc2e083b58msh94959c04b8fc1c4p1017e1jsna6f447753c4e',
+                'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
+            }
+        };
 
-let ws ={
-    async showAll(){
-        const data = await api.getData();
-        let html=''
-        data.forEach(game => {
+        try {
+            const response = await fetch(url, options);
+            const data = await response.json();
+            return data
+        } catch (error) {
+            console.log(error);
+        }
+    },
+    render_templates(arr_data) {
+        let div;
+        arr_data.slice(0, 1).forEach(game => {
             const {
                 developer,
                 freetogame_profile_url,
@@ -17,22 +33,37 @@ let ws ={
                 thumbnail,
                 title
             } = game;
-            html+=`
+            div = `
             <div>${title}</div>
-        <div><img id="img" src="${thumbnail}"></div>
-        <div>${short_description}</div>
-        <div>${publisher}</div>
-        <div>${release_date}</div>
-        <div>${platform}</div>
-        <div>${genre}</div>
-        <div>${id}</div>
-        <div>${developer}</div>
-        <div><a href="${freetogame_profile_url}">LINK AL JUEGO</a></div><br>
-            `
-    });
-    return [html]
+            <div><img src="${thumbnail}"></div>
+            <div>${short_description}</div>
+            <div>${publisher}</div>
+            <div>${release_date}</div>
+            <div>${platform}</div>
+            <div>${genre}</div>
+            <div>${id}</div>
+            <div>${developer}</div>
+            <div><a href="${freetogame_profile_url}">LINK AL JUEGO</a></div><br>`;
+        });
+        return div
+    },
+    butons() {
+        let anterior = document.getElementById('but1');
+        anterior.addEventListener('click', async () => {
+            offset -= 1; // Actualizar el offset
+            await ws.consult(); 
+        })
+        let siguiente = document.getElementById('but');
+        siguiente.addEventListener('click', async () => {
+            offset += 1; // Actualizar el offset
+            await ws.consult();
+        });
     }
 }
-self.addEventListener("message",(e)=>{
-    postMessage(ws[`${e.data.module}`](e.data.data));
+
+
+self.addEventListener('message', async (r) => { //enviamos la informacion
+    let res_consult = await ws.consult() //almacenamos el array que retorna la api
+    let res_template = ws.render_templates(res_consult) //creamos nueva variable que recibe el parametro del array y lo inseta en la funcion
+    postMessage(res_template) //enviamos la informacion al worker de la app.js
 })
